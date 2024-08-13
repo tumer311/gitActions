7422c50a9362d9863f8b1eb45bb64b7cb217a6f2
Bekir <36570425+tumer311@users.noreply.github.com>
2024-08-14 01:54:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,42 @@
-def hello():
-    print("hi")
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
+givenTestData = ["NFLX","MSFT", "TSLA"]
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get("https://www.google.com/finance/")
+""" symbols = driver.find_element(By.XPATH, "//section[@aria-labelledby='smart-watchlist-title']//div[contains(@class, 'COaKTb')]") """
+stocks = driver.find_elements(By.XPATH, "//section[@aria-labelledby='smart-watchlist-title']//div[contains(@class, 'COaKTb')]")
+q3 = []
+q4 = []
+assertion_results = []
+for stock in stocks:
+    if stock not in givenTestData:
+        q3.append(stock.text)
 
-def bye():
-    print("bye")
+for testStock in givenTestData:
+    if testStock not in stocks:
+        q4.append(testStock)
 
+pageTitle = driver.title
 
-print(hello())
+try:
+    try:
+       assert 'Google Finance' in pageTitle
+    except AssertionError as e:
+        assertion_results.append(f"Assertion failed for page title: {str(e)}")
+    try:
+        for testStock in givenTestData:
+            for stock in stocks:
+                assert stock.text == testStock
+    except AssertionError as e:
+        assertion_results.append(f"Assertion failed for the two list: {str(e)}")
+
+finally:
+    # Print all assertion results after the test completes
+    for result in assertion_results:
+        print(result)
+
+print(q3)
+print(q4)
+
